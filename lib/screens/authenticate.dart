ec83d2fd726375ca7d1dc42af4c6c7c88d9c2d56
siswacpt025 <siswacpt025@student.wethinkcode.co.za>
2026-09-03 13:05:55 +0200

    feat: Implement authentication state toggle between Login and Register

--- lib/screens/authenticate.dart
+++ lib/screens/authenticate.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'register_screen.dart';
 
-class AuthenticateScreen extends StatelessWidget {
+class AuthenticateScreen extends StatefulWidget {
   const AuthenticateScreen({super.key});
+
+  @override
+  State<AuthenticateScreen> createState() => _AuthenticateScreenState();
+}
+
+class _AuthenticateScreenState extends State<AuthenticateScreen> {
+  // By default, show the Login screen first
+  bool showSignIn = true;
+
+  // Function to toggle the state
+  void toggleView() {
+    setState(() => showSignIn = !showSignIn);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Authenticate Screen')));
+    if (showSignIn) {
+      // Pass the toggle function down to LoginScreen
+      return LoginScreen(onToggleToRegister: toggleView);
+    } else {
+      // Pass the toggle function down to RegisterScreen
+      return RegisterScreen(onToggleToLogin: toggleView);
+    }
   }
 }
